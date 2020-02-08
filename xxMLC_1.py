6b578bb1e75bebb4caeb081c8a7f0d0d003d51f7
Nikita Zavartsev <nikoss9644@gmail.com>
2020-02-08 17:09:37 +0600

    first funcionality works

--- xxMLC_1.py
+++ xxMLC_1.py
@@ -32,6 +32,8 @@ class Prog_comp_1(QtWidgets.QMainWindow):
     super().__init__()
     self.ui = Ui_MainWindow()
     self.ui.setupUi(self)
+    # event for button "Расчет"
+    self.ui.btn_count.clicked.connect(self.count_main)
 
   def closeEvent(self, e):
     """This method require an affirmation on close event"""
@@ -43,6 +45,62 @@ class Prog_comp_1(QtWidgets.QMainWindow):
       e.accept()
     else:
       e.ignore()
+
+  # btn_count click function
+  def count_main(self):
+    # count for "Штатная" colib_set
+    q = str(self.ui.comboBox_5.currentText())
+    if q == 'Штатная':
+      ## a = get_colib_from_ex_1()
+      # get values from interface
+      f_D1 = (self.ui.ent_D1.currentText())
+      f_Doh = self.ui.ent_Doh.currentText()
+      f_T = self.ui.ent_T.text()
+      f_beta = self.ui.ent_beta.currentText()
+      f_Dvp = self.ui.ent_Dvp.text()
+      f_nd = self.ui.ent_nd.text()
+      key = str(self.ui.comboBox_4.currentText())
+      print("q")
+      # use math_model to get required values PC_1 and PC_2
+      arr_out_math = Model_MLC.math_model(f_D1, f_Doh, f_T, f_beta, f_Dvp, f_nd, key)
+      if arr_out_math[0] != 1111:
+        # 1111 is appended to the array in case of an error
+        # the next value in array (index = 1) is the error message
+        self.ui.get_PC1.setText(str(arr_out_math[0]))
+        self.ui.get_PC2.setText(str(arr_out_math[1]))
+      else:
+        # error message in case of an error
+        QtWidgets.QMessageBox.warning(self, "Ошибка", arr_out_math[1],
+                                      QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
+    else:
+      ## a = get_colib_from_ex_1()
+      # get values from interface
+      f_D1 = (self.ui.ent_D1.currentText())
+      f_Doh = self.ui.ent_Doh.currentText()
+      f_T = self.ui.ent_T.text()
+      f_beta = self.ui.ent_beta.currentText()
+      f_Dvp = self.ui.ent_Dvp.text()
+      f_nd = self.ui.ent_nd.text()
+      key = str(self.ui.comboBox_4.currentText())
+      print("q")
+      # use math_model to get required values PC_1 and PC_2
+      arr_out_math = Model_MLC.math_model(f_D1, f_Doh, f_T, f_beta, f_Dvp, f_nd,
+                                          key, self.steels.get_obj_steels())
+      if arr_out_math[0] != 1111:
+        # 1111 is appended to the array in case of an error
+        # the next value in array (index = 1) is the error message
+        self.ui.get_PC1.setText(str(arr_out_math[0]))
+        self.ui.get_PC2.setText(str(arr_out_math[1]))
+      else:
+        # error message in case of an error
+        QtWidgets.QMessageBox.warning(self, "Ошибка", arr_out_math[1],
+                                      QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
+      print('not yet')
+
+
+
+
+
 
 
 def main():
