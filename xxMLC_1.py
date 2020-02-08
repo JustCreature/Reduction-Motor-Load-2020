867f293f78ec958eedd507cccab82168736bb94e
Nikita Zavartsev <nikoss9644@gmail.com>
2020-02-08 18:00:30 +0600

    create get_ex fuction

--- xxMLC_1.py
+++ xxMLC_1.py
@@ -28,6 +28,55 @@ from math_model import Model_MLC
 
 
 class Prog_comp_1(QtWidgets.QMainWindow):
+  # Dict with all parameters
+  @staticmethod
+  def get_colib_from_ex_2():
+    calka = {}
+    wb = xlrd.open_workbook('for_Dict_2.xlsx')
+    sh = wb.sheet_by_index(0)
+    n = 2
+
+    # Add all for a
+    for ind_colib_prop in range(1, 4):  # layer for properties (a, b, c)
+      # choose property
+      if ind_colib_prop == 1:
+        colib_prop = '_a'
+      elif ind_colib_prop == 2:
+        colib_prop = '_b'
+      elif ind_colib_prop == 3:
+        colib_prop = '_c'
+      # choose "beta" angle
+      for n in range(2, 57):
+        if n < 9:
+          beta = 14
+        elif n < 16:
+          beta = 13
+        elif n < 23:
+          beta = 12
+        elif n < 30:
+          beta = 11
+        elif n < 37:
+          beta = 10
+        elif n < 44:
+          beta = 9
+        elif n < 51:
+          beta = 8
+        elif n < 57:
+          beta = 7
+        # add all property's values to the dict {calka}
+        for i in range(n, n + 5):
+          if str(sh.cell(n, 0).value) == '':
+            continue
+          key_name = str(sh.cell(n, 0).value) + str(beta) + colib_prop
+          key_value = sh.cell(n, ind_colib_prop).value
+          calka[key_name] = float(key_value)
+          i += 1
+
+        n += 7
+      ind_colib_prop += 1
+    print(calka)
+    return calka
+
   def __init__(self):
     super().__init__()
     self.ui = Ui_MainWindow()
